01b9a14b1988265b9100eda10091fccf79f1b706
Abdullah Anwar <info.ibnanwar@gmail.com>
2024-07-02 16:17:53 +0500

    Fetch Data from Server

---
+++ lib/Services/state_services.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:covid_19_mob_app/Services/Utilities/app_url.dart';
+import 'package:http/http.dart' as http;
+import '../Model/world_states_model.dart';
+
+class StateServices{
+
+  Future<WorldStatesModel> fetchWorldStatesApi()async {
+    final response=await http.get(Uri.parse(AppUrl.worldStatesApi));
+    if(response.statusCode==200){
+
+      var data =jsonDecode(response.body.toString());
+      return WorldStatesModel.fromJson(data);
+
+    }
+    else{
+        throw(Exception("Error"));
+    }
+}
+
+}
--- lib/view/word_states_screen.dart
+++ lib/view/word_states_screen.dart
@@ -1,8 +1,9 @@
+import 'package:covid_19_mob_app/Model/world_states_model.dart';
+import 'package:covid_19_mob_app/Services/state_services.dart';
 import 'package:covid_19_mob_app/constant/const_common_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
 import '../componets/row_comonent.dart';
-
 
 class WordStatesScreen extends StatefulWidget {
   const WordStatesScreen({super.key});
@@ -12,6 +13,8 @@ class WordStatesScreen extends StatefulWidget {
 }
 
 class _WordStatesScreenState extends State<WordStatesScreen> {
+  StateServices stateServices = StateServices();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,66 +24,89 @@ class _WordStatesScreenState extends State<WordStatesScreen> {
       //   title: Text("COVID-19 Tracker"),
       // ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
-          SizedBox(height: 80,),
-            PieChart(
-              dataMap:const{
-                'Total':20,
-                'Recovered':300,
-                'Death':200
-              } ,
-              animationDuration: Duration(milliseconds: 800),
-              chartLegendSpacing: 32,
-              chartRadius: MediaQuery.of(context).size.width / 2.7,
-              colorList: commonColors,
-              initialAngleInDegree: 0,
-              chartType: ChartType.ring,
-              ringStrokeWidth: 32,
-              legendOptions: LegendOptions(
-                showLegendsInRow: false,
-                legendPosition: LegendPosition.right,
-                showLegends: true,
-                legendShape: BoxShape.circle,
-                legendTextStyle: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              chartValuesOptions: ChartValuesOptions(
-                showChartValueBackground: true,
-                showChartValues: true,
-                showChartValuesInPercentage: true,
-                showChartValuesOutside: false,
-                decimalPlaces: 1,
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Card(
-              color: Colors.orange,
-              child: Column(
-                children: [
-                  ReusableRow(title: "Total", value: "200"),
-                  Divider(color: Colors.grey),
-                  ReusableRow(title: "Total", value: "200"),
-                  Divider(color: Colors.grey),
-                  ReusableRow(title: "Total", value: "200"),
-                ],
-              ),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 12.0),
-              ),
-              onPressed: () {
-                // Add navigation logic here
+            FutureBuilder(
+              future: stateServices.fetchWorldStatesApi(),
+              builder: (context, AsyncSnapshot<WorldStatesModel> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else {
+                  return Column(
+                    children: [
+                      SizedBox(height: 80),
+                      PieChart(
+                        dataMap: {
+                          'Total': double.parse(snapshot.data!.cases.toString()),
+                          'Recovered': double.parse(snapshot.data!.recovered.toString()),
+                          'Death': double.parse(snapshot.data!.deaths.toString()),
+                        },
+                        animationDuration: Duration(milliseconds: 800),
+                        chartLegendSpacing: 32,
+                        chartRadius: MediaQuery.of(context).size.width / 2.7,
+                        colorList: commonColors,
+                        initialAngleInDegree: 0,
+                        chartType: ChartType.ring,
+                        ringStrokeWidth: 32,
+                        legendOptions: LegendOptions(
+                          showLegendsInRow: false,
+                          legendPosition: LegendPosition.left,
+                          showLegends: true,
+                          legendShape: BoxShape.circle,
+                          legendTextStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        chartValuesOptions: ChartValuesOptions(
+                          showChartValueBackground: true,
+                          showChartValues: true,
+                          showChartValuesInPercentage: true,
+                          showChartValuesOutside: false,
+                          decimalPlaces: 1,
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      Card(
+                        color: Colors.orange,
+                        child: Column(
+                          children: [
+                            ReusableRow(title: "Total Cases", value: snapshot.data!.cases.toString()),
+                            Divider(color: Colors.grey),
+                            ReusableRow(title: "Recovered", value: snapshot.data!.recovered.toString()),
+                            Divider(color: Colors.grey),
+                            ReusableRow(title: "Deaths", value: snapshot.data!.deaths.toString()),
+                            Divider(color: Colors.grey),
+                            ReusableRow(title: "Today Recorded", value: snapshot.data!.todayRecovered.toString()),
+                            Divider(color: Colors.grey),
+                            ReusableRow(title: "Population", value: snapshot.data!.population.toString()),
+                            Divider(color: Colors.grey),
+                            ReusableRow(title: "Critical", value: snapshot.data!.critical.toString()),
+                            Divider(color: Colors.grey),
+                            ReusableRow(title: "Affected Countries", value: snapshot.data!.affectedCountries.toString()),
+                          ]
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 12.0),
+                        ),
+                        onPressed: () {
+                          // Add navigation logic here
+                        },
+                        child: Text(
+                          "Track Countries",
+                          style: TextStyle(color: Colors.white, fontSize: 16.0),
+                        ),
+                      ),
+                    ],
+                  );
+                }
               },
-              child: Text(
-                "Track Countries",
-                style: TextStyle(color: Colors.white, fontSize: 16.0),
-              ),
             ),
           ],
         ),
