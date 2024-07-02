79977f29b6c27942eab77b735c1429f05db46b2c
Abdullah Anwar <info.ibnanwar@gmail.com>
2024-07-02 12:04:20 +0500

    Splash Screen Created

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:covid_19_mob_app/view/word_states_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
@@ -13,6 +16,18 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
   late final AnimationController _controller=AnimationController(
   duration: Duration(seconds: 3),
       vsync: this)..repeat();
+  
+  @override
+  void initState() {
+    super.initState();
+    // Start a timer to navigate to WordStatesScreen after 5 seconds
+    Timer(Duration(seconds: 5), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => WordStatesScreen()),
+      );
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +35,19 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-AnimatedBuilder(animation: _controller, builder: (BuildContext context, Widget?child){
+AnimatedBuilder(animation: _controller,
+    child: Center(
+      child: Container(
+        height: 200,
+        width: 200,
+        child: const Image(image: AssetImage("assets/images/virus.png")),
+      ),
+    ),builder: (BuildContext context, Widget?child){
   return Transform.rotate(angle: _controller.value *2.0 * math.pi ,child: child);
 }),
+          SizedBox(height:  MediaQuery.of(context).size.height *.08,),
+           Text(
+           "Covid 19\nTracking App",style: TextStyle(fontSize: 40),textAlign: TextAlign.center,),
         ],
       )),
     );
---
+++ lib/view/word_states_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WordStatesScreen extends StatefulWidget {
+  const WordStatesScreen({super.key});
+
+  @override
+  State<WordStatesScreen> createState() => _WordStatesScreenState();
+}
+
+class _WordStatesScreenState extends State<WordStatesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('MainBody'),),
+    );
+  }
+}
+
